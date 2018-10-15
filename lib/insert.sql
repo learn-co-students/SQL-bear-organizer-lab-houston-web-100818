c8591a0e60ab10617d1d11d81d7e7773fdca10e5
Robert Schulte <schulte.robertw@outlook.com>
2018-10-15 14:51:54 -0500

    created table, inserted data, wrote queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "Male", "Yellow", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "Male", "Yellow", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 4, "Female", "Yellow", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 2, "Male", "Yellow", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "Female", "Yellow", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 3, "Male", "Yellow", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 8, "Female", "Yellow", "Sad", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, NULL, "Male", "Yellow", "Sad", 1);
